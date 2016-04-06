c5b5a9040b6a5b7f3cadb972d01be997b9ad7cf1
LegendIsAwesomes <beanben@outlook.com>
2016-04-06 19:02:16 +0100

    add other developers
    add other developers

--- src/main/java/me/totalfreedom/totalfreedommod/util/FUtil.java
+++ src/main/java/me/totalfreedom/totalfreedommod/util/FUtil.java
@@ -61,7 +61,7 @@ public class FUtil
     public static final String SAVED_FLAGS_FILENAME = "savedflags.dat";
     public static final Map<String, EntityType> MOB_TYPES = new HashMap<>();
     // See https://github.com/TotalFreedom/License - None of the listed names may be removed.
-    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "Wild1145", "WickedGamingUK", "AndySixx", "Adaptified");
+    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "Wild1145", "WickedGamingUK", "AndySixx", "Adaptified", "OxLemonxO");
     public static String DATE_STORAGE_FORMAT = "EEE, d MMM yyyy HH:mm:ss Z";
     public static final Map<String, ChatColor> CHAT_COLOR_NAMES = new HashMap<>();
     public static final List<ChatColor> CHAT_COLOR_POOL = Arrays.asList(
